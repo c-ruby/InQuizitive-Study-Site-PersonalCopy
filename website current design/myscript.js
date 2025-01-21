37f0324f131bda01f4f637c47b373c2eeba87396
Dataplays36 <167057851+Dataplays36@users.noreply.github.com>
2025-01-21 14:20:11 -0500

    Add files via upload

--- website current design/myscript.js
+++ website current design/myscript.js
@@ -9,7 +9,7 @@ function clickPress(event){
 will use the following function for testing
 and to see if the user exists in the database
 */
-/*
+
 //this function is to see if there is an account for the website
 //will eventually have it get rid of the login/signup button 
 //and display a Welcome "username" or something along those lines
@@ -17,13 +17,12 @@ and to see if the user exists in the database
 function checkforaccount(){
 	var userName;
 	var passWord;
-	
-	alert('test');
 }
-*/
+
 function getInformation(){
 	var userName = document.getElementById("username").value;
 	var passWord = document.getElementById("password").value;
+	alert(userName);
 }
 
 
