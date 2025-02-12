79f3c855ff67e2fe5c95fe98104ce9cd5874194c
Dataplays36 <crachocki@gmail.com>
2025-02-11 23:24:26 -0500

    updated template
    added basic functionality to studyset quizzes

--- website current design/myscript.js
+++ website current design/myscript.js
@@ -3,14 +3,31 @@ This is simply to clone the rows on the study set template page
 Will update when DB is set up to auto set rows for how many questions are in the set
 for now just clones the table row
 */
+
+
+function getRowCount(){
+	const table = document.getElementById('myTable');
+    return table.rows.length-1;
+}
+
+let count=1;
+
 function cloneRow() {
-	var html = document.getElementById("therow").innerHTML;
-
-	var row = document.createElement('tr');
-
-	row.innerHTML= html;
-
-	document.getElementById("myTable").appendChild(row);
+	const table = document.getElementById("myTable");
+    const row = table.insertRow();
+    const question = row.insertCell();
+	const answer = row.insertCell();
+	
+	row.setAttribute('class', 'StudySet-Q&A');
+	row.setAttribute('id', 'row'+count);
+	
+    question.textContent = count;
+	answer.textContent = count;
+	
+	question.setAttribute('id', 'question' + count);
+    answer.setAttribute('id', 'answer' + count);
+	
+	count++;
 }
 
 // simulates a simple user database
@@ -101,18 +118,57 @@ function clickPress(event){
 }
 
 function generate_quiz(){
-	
-	document.getElementById('SSheader').style.opacity = '.2';
-	document.getElementById('SScontent').style.opacity = '.2';
-	document.getElementById('SSfooter').style.opacity = '.2';
+	//These set the opacity of the elements on the page to low
+	document.getElementById('SSheader').style.opacity = '.1';
+	document.getElementById('SScontent').style.opacity = '.1';
+	document.getElementById('SSfooter').style.opacity = '.1';
 
-	
+	//this adds the name of the study set to the top of the test
+	var studySetName = document.getElementById("studySetName").innerHTML;
 	var quiz = document.createElement("div");
+	studySetName.id = "StudySetName"
 	quiz.id = "Quiz";
-	quiz.innerHTML = "TEST";
+	quiz.innerHTML = studySetName;
 	document.body.appendChild(quiz);
-	
 	quiz.style.opacity = '1';
+	
+	//this adds the submit button to the bottom of the quiz div
+	var submitbtn = document.createElement("button");
+    submitbtn.id = "submitbtn";
+    submitbtn.innerHTML = "Submit";
+	submitbtn.onclick = disablequiz;
+    quiz.appendChild(submitbtn);
+	
+	//These disable all of the buttons on the side when the quiz is generated
+	document.getElementById('flashcardbtn').disabled = true;
+	document.getElementById('quizbtn').disabled = true;
+	document.getElementById('settingsbtn').disabled = true;
+	document.getElementById('editbtn').disabled = true;
+	document.getElementById('addrowbtn').disabled = true;
+		
+	//This code is what generates the quiz questions and answers	
+	var formforquiz = document.createElement("form");
+	var questionLbl = document.createElement("label");
+	var questionAnswer = document.createElement("")
+	
+}
+
+function disablequiz() {
+    document.getElementById('SSheader').style.opacity = '1';
+    document.getElementById('SScontent').style.opacity = '1';
+    document.getElementById('SSfooter').style.opacity = '1';
+
+    var quiz = document.getElementById("Quiz");
+    if (quiz) {
+        quiz.parentNode.removeChild(quiz);
+    }
+
+    // Enable buttons
+    document.getElementById('flashcardbtn').disabled = false;
+    document.getElementById('quizbtn').disabled = false;
+    document.getElementById('settingsbtn').disabled = false;
+    document.getElementById('editbtn').disabled = false;
+    document.getElementById('addrowbtn').disabled = false;
 }
 
 function flashcards(table){
