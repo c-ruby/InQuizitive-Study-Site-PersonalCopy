d16d40fbb09dabfead3a19f72f3ea2110acb9f3a
C-Mass-03 <123325378+C-Mass-03@users.noreply.github.com>
2025-04-16 19:45:12 -0400

    About Tutorials / Explore Edits

--- root/frontend/js/profile.js
+++ root/frontend/js/profile.js
@@ -1,9 +1,3 @@
-// Script to update user-info on profile page
-document.getElementById("userName").innerHTML = userInfo.name;
-document.getElementById("userJoinDate").innerHTML = userInfo.joinDate;
-
-
-
 //script to handle logging out 
 document.addEventListener('DOMContentLoaded', (event) => {
     const deleteAccountBtn = document.getElementById('logoutBtn');
